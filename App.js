d3c8b2e4482ee4000e5a8fe43ce49430a3ea8c98
max3d <vadim@unikivi.ee>
2019-07-07 01:31:27 +0300

    fetch data using axios

--- App.js
+++ App.js
@@ -2,35 +2,19 @@ import React from 'react';
 import {StyleSheet, Text, TextInput, View, FlatList} from 'react-native';
 import Header from './src/components/header.js';
 import RestaurantRow from './src/components/restaurant-row.js';
-
-const restaurants = [
-    {name: 'React Cafe', address: '123 Anywhere St'},
-    {name: 'Fancy Restaurant', address: '799 Main St'},
-    {name: 'Taco Place', address: '550 Maple Rd'},
-    {name: 'Tony\'s Diner', address: '4101 College St'},
-    {name: 'Pasta Central', address: '706 Harper St'},
-    {name: 'Burger Builder', address: '4869 Hamilton Dr'},
-    {name: 'Pizza Express', address: '1049 Bird St'},
-    {name: 'Teriyaki To Go', address: '1885 Tea Berry Lane'},
-    {name: 'Maroon Deli', address: '1082 Stuart St'},
-    {name: 'Prime Bar and Grill', address: '1848 Fairfax Dr'},
-    {name: 'Dumpling House', address: '747 Kelly Dr'},
-    {name: 'Hot Chicken', address: '1816 Olive St'},
-    {name: 'Luna\'s Tap Room', address: '3256 Spirit Dr'},
-    {name: 'Quick Sandwich Shop', address: '2587 Cherry Ridge Dr'},
-    {name: 'Bobby\'s Burgers', address: '4152 Berkley St'},
-    {name: 'Turnpike Diner', address: '4571 Central Ave'},
-    {name: 'Bombay Express', address: '65 Queens Lane'},
-    {name: 'Coffee Central', address: '3228 Oakwood Circle'},
-    {name: 'King\'s Garden', address: '2935 Victoria Ct'},
-    {name: 'Salads and More', address: '2454 Preston St'},
-];
+import axios from "axios";
 
 class App extends React.Component {
 
     state = {
         search: null,
+        restaurants: [],
     };
+
+    componentDidMount() {
+        axios.get('http://192.168.1.171:3000/restaurants')
+            .then(result => this.setState({restaurants: result.data}));
+    }
 
     render() {
         console.log('search', this.state.search);
@@ -49,7 +33,7 @@ class App extends React.Component {
 
             <FlatList
 
-                data={restaurants.filter(place => !this.state.search ||
+                data={this.state.restaurants.filter(place => !this.state.search ||
                     place.name.toLowerCase().indexOf(this.state.search.toLowerCase()) > -1)}
 
                 renderItem={({item, index}) => <RestaurantRow index={index} place={item}/>}
