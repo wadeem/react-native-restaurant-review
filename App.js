c23a559d778850ded08bc5bc7884761258837575
max3d <vadim@unikivi.ee>
2019-07-06 23:21:31 +0300

    header extracted. Scroll view added

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import {StyleSheet, Text, TextInput, View} from 'react-native';
-import headerStyle from './header-style.js';
+import {StyleSheet, Text, TextInput, View, ScrollView} from 'react-native';
+import Header from './src/components/header.js';
 
 const restaurants = [
     {name: 'React Cafe', address: '123 anywhere str'},
     {name: 'Awesome pizza', address: '799 main str'},
     {name: 'Taco place', address: '33 silver rd'},
+    {name: 'Burger prince', address: '123 happy rd'},
+    {name: 'BBQ friends', address: '78 big str'},
 ];
 
 class App extends React.Component {
@@ -15,10 +17,11 @@ class App extends React.Component {
     };
 
     render() {
+        console.log('search', this.state.search);
 
         return <View style={{flex: 1}}>
 
-            <Text style={headerStyle.header}>Restaurant Review</Text>
+            <Header/>
 
             <TextInput style={styles.input}
                        placeholder="Live Search"
@@ -28,28 +31,28 @@ class App extends React.Component {
                        value={this.state.search}
             />
 
+            <ScrollView>
+                {restaurants.filter(place => !this.state.search ||
+                    place.name.toLowerCase().indexOf(this.state.search.toLowerCase()) > -1)
+                    .map((place, index) =>
 
-            {restaurants.filter(place => !this.state.search ||
-                place.name.toLowerCase().indexOf(this.state.search.toLowerCase()) > -1)
-                .map((place, index) =>
+                        <View key={place.name} style={
+                            [styles.row, {backgroundColor: index % 2 === 0 ? 'white' : '#f3f3f7'}]
+                        }>
 
-                    <View key={place.name} style={
-                        [styles.row, {backgroundColor: index % 2 === 0 ? 'white' : '#f3f3f7'}]
-                    }>
-
-                        <View style={styles.edges}>
-                            <Text>{index + 1}</Text>
-                        </View>
-                        <View style={styles.nameAddress}>
-                            <Text>{place.name}</Text>
-                            <Text style={styles.address}>{place.address}</Text>
-                        </View>
-                        <View style={styles.edges}>
-                            <Text>Info</Text>
-                        </View>
-                    </View>,
-                )}
-
+                            <View style={styles.edges}>
+                                <Text>{index + 1}</Text>
+                            </View>
+                            <View style={styles.nameAddress}>
+                                <Text>{place.name}</Text>
+                                <Text style={styles.address}>{place.address}</Text>
+                            </View>
+                            <View style={styles.edges}>
+                                <Text>Info</Text>
+                            </View>
+                        </View>,
+                    )}
+            </ScrollView>
         </View>;
 
     }
---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Text} from 'react-native';
+import headerStyle from '../styles/header-style.js';
+
+const Header = () => {
+    return <Text style={headerStyle.header}>Restaurant Review</Text>;
+};
+
+export default Header;
