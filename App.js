be28474e9ddfe46c80b5943be53fd6600082199b
max3d <vadim@unikivi.ee>
2019-07-06 20:51:26 +0300

    text sections included to view elements with styling

--- App.js
+++ App.js
@@ -12,17 +12,24 @@ class App extends React.Component {
 
     render() {
 
-        return <View>
+        return <View style={{flex: 1}}>
             <Text style={{
                 padding: 40, fontSize: 30,
                 color: '#0066cc', fontWeight: '300', textAlign: 'center',
             }}>Restaurant Review</Text>
 
-            {restaurants.map((place, index) => <View key={place.name}>
-                    <Text>{index + 1}</Text>
-                    <Text>{place.name}</Text>
-                    <Text style={{color: 'grey'}}>{place.address}</Text>
-                    <Text>Info</Text>
+            {restaurants.map((place, index) =>
+                <View key={place.name} style={{flexDirection: 'row'}}>
+                    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+                        <Text>{index + 1}</Text>
+                    </View>
+                    <View style={{flex: 8, flexDirection: 'column'}}>
+                        <Text>{place.name}</Text>
+                        <Text style={{color: 'grey'}}>{place.address}</Text>
+                    </View>
+                    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                        <Text>Info</Text>
+                    </View>
                 </View>,
             )}
 
