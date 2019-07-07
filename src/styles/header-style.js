458ef3689816299d6023e7346f78e2d6eecd04d0
max3d <vadim@unikivi.ee>
2019-07-07 22:07:33 +0300

    react-native-vector-icons

--- src/styles/header-style.js
+++ src/styles/header-style.js
@@ -3,6 +3,7 @@ import {StyleSheet} from 'react-native';
 const headerStyle = StyleSheet.create({
     header: {
         padding: 40,
+        paddingTop: 0,
         fontSize: 30,
         color: '#0066cc',
         fontWeight: '300',
